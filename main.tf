65b19dea7b31a095ac0267e20e542584bf06c2bf
Reddylab36 <shekharreddy36@outlook.com>
2024-10-14 15:16:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,63 +1,97 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
+  access_key = var.YOUR_ACCESS_KEY
+  secret_key = var.YOUR_SECRET_KEY
+}
+resource "aws_security_group" "vm_sg" {
+  name        = "vm_sg2"
+  description = "Security group for VMs"
+  
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 19999
+    to_port     = 19999
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "backend" { #ubuntu.yaml NETADATA
-  ami                    = "ami-0fc5d935ebf8bc3bc"
-  instance_type          = "t2.micro" 
-  key_name               = "ubuntu-key"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
+# Deploy first VM (Amazon Linux)
+resource "aws_instance" "c8_local" {
+  ami           = var.linux-ami # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = "ansible" # Specify your key pair
+  security_groups = [aws_security_group.vm_sg.name]
+  associate_public_ip_address = true
+
   tags = {
-    Name = "u21.local"
+    Name = "c8_local"
   }
-  user_data = <<-EOF
-  #!/bin/bash
-  sudo hostnamectl set-hostname U21.local
-  # netdata_conf="/etc/netdata/netdata.conf"
+
+  user_data = <<EOF
+#!/bin/bash
+sudo hostnamectl set-hostname c8_local
+  hostname=$(hostname)
+  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
+
+  # Path to /etc/hosts
+  echo "${aws_instance.u22_local.public_ip} $hostname" | sudo tee -a /etc/hosts
+
+EOF
+depends_on = [aws_instance.c8_local]
+
+}
+
+# Deploy second VM (Ubuntu 21.04)
+resource "aws_instance" "u22_local" {
+  ami           = var.ubuntu-ami # Ubuntu Server 22.04 AMI
+  instance_type = "t2.micro"
+  key_name      = "ansible" # Specify your key pair
+  security_groups = [aws_security_group.vm_sg.name]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "u22_local"
+  }
+
+  user_data = <<EOF
+#!/bin/bash
+sudo hostnamectl set-hostname u22_local
+netdata_conf="/etc/netdata/netdata.conf"
   # Path to netdata.conf
   # actual_ip=0.0.0.0
   # Use sed to replace the IP address in netdata.conf
   # sudo sed -i "s/bind socket to IP = .*$/bind socket to IP = $actual_ip/" "$netdata_conf"
 EOF
-
-}
-
-resource "aws_instance" "frontend" { #amazon-playbook.yaml NGINX
-  ami                    = "ami-05c13eab67c5d8861"
-  instance_type          = "t2.micro"
-  key_name               = "linux"
-  vpc_security_group_ids = ["sg-04cb5f715d70d6f35"]
-  tags = {
-    Name = "c8.local"
-  }
-  user_data = <<-EOF
-  #!/bin/bash
-  # New hostname and IP address
-  sudo hostnamectl set-hostname u21.local
-  hostname=$(hostname)
-  public_ip="$(curl -s https://api64.ipify.org?format=json | jq -r .ip)"
-
-  # Path to /etc/hosts
-  echo "${aws_instance.backend.public_ip} $hostname" | sudo tee -a /etc/hosts
-
-EOF
-depends_on = [aws_instance.backend]
 }
 
 resource "local_file" "inventory" {
   filename = "./inventory.yaml"
   content  = <<EOF
 [frontend]
-${aws_instance.frontend.public_ip}
+${aws_instance.c8_local.public_ip}
 [backend]
-${aws_instance.backend.public_ip}
+${aws_instance.u22_local.public_ip}
 EOF
 }
 
 output "frontend_public_ip" {
-  value = aws_instance.frontend.public_ip
+  value = aws_instance.c8_local.public_ip
 }
 
 output "backend_public_ip" {
-  value = aws_instance.backend.public_ip
+  value = aws_instance.u22_local.public_ip
 }
